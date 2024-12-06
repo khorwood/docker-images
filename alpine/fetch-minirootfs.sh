14c1489c6ac7f81849b130941e29ff3f581be449
Kristian Horwood <khorwood@gmail.com>
2024-12-06 17:02:56 -0500

    Upgrade alpine to 3.21.0 (#30)

--- alpine/fetch-minirootfs.sh
+++ alpine/fetch-minirootfs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-RELEASE=alpine-minirootfs-3.20.3-x86_64
-curl -sSO https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/$RELEASE.tar.gz
-curl -sSO https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/$RELEASE.tar.gz.sha256
+RELEASE=alpine-minirootfs-3.21.0-x86_64
+curl -sSO https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/$RELEASE.tar.gz
+curl -sSO https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/$RELEASE.tar.gz.sha256
 sha256sum -c $RELEASE.tar.gz.sha256
