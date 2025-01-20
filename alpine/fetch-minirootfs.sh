fb55f87bb4127961ee234f43d93893a193012fc9
Kristian Horwood <khorwood@gmail.com>
2025-01-20 17:36:57 -0500

    Reduce toil for alpine image updates

--- alpine/fetch-minirootfs.sh
+++ alpine/fetch-minirootfs.sh
@@ -3,3 +3,4 @@ RELEASE=alpine-minirootfs-3.21.2-x86_64
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/$RELEASE.tar.gz
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/$RELEASE.tar.gz.sha256
 sha256sum -c $RELEASE.tar.gz.sha256
+mv $RELEASE.tar.gz alpine-minirootfs.tar.gz
