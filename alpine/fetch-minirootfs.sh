3484b1ff649f1d73c86d7d8bbc6d83374aab6c3d
Kristian Horwood <khorwood@gmail.com>
2025-02-15 23:12:38 -0500

    Bump alpine to v3.21.3 (#59)

--- alpine/fetch-minirootfs.sh
+++ alpine/fetch-minirootfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 VERSION=v3.21
-RELEASE=alpine-minirootfs-3.21.2-x86_64
+RELEASE=alpine-minirootfs-3.21.3-x86_64
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/$VERSION/releases/x86_64/$RELEASE.tar.gz
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/$VERSION/releases/x86_64/$RELEASE.tar.gz.sha256
 sha256sum -c $RELEASE.tar.gz.sha256
