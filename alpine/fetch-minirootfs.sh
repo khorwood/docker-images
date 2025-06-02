b64a6495893c258715a3063a3e7c9e2b9218143d
Kristian Horwood <khorwood@gmail.com>
2025-06-02 18:06:17 -0400

    Update alpine to v3.22.0 (#84)

--- alpine/fetch-minirootfs.sh
+++ alpine/fetch-minirootfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
-VERSION=v3.21
-RELEASE=alpine-minirootfs-3.21.3-x86_64
+VERSION=v3.22
+RELEASE=alpine-minirootfs-3.22.0-x86_64
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/$VERSION/releases/x86_64/$RELEASE.tar.gz
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/$VERSION/releases/x86_64/$RELEASE.tar.gz.sha256
 sha256sum -c $RELEASE.tar.gz.sha256
