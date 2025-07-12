e532a7803846d437c682f5e9710181440298c9f3
Kristian Horwood <khorwood@gmail.com>
2025-07-12 01:01:23 -0400

    Move images to parameterized builds (#92)

--- alpine/fetch-minirootfs.sh
+++ alpine/fetch-minirootfs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
-VERSION=v3.22
-RELEASE=alpine-minirootfs-3.22.0-x86_64
+VERSION=$1
+RELEASE=alpine-minirootfs-$2-x86_64
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/$VERSION/releases/x86_64/$RELEASE.tar.gz
 curl -sSO https://dl-cdn.alpinelinux.org/alpine/$VERSION/releases/x86_64/$RELEASE.tar.gz.sha256
 sha256sum -c $RELEASE.tar.gz.sha256
+rm $RELEASE.tar.gz.sha256
 mv $RELEASE.tar.gz alpine-minirootfs.tar.gz
